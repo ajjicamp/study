a0731b7841631e85a38c6168f2c2bdc163610bad
ajjicamp <cwhur0616@gmail.com>
2021-09-22 22:56:10 +0900

    9/22 commit

--- shared/sharemanage.py
+++ shared/sharemanage.py
@@ -1,73 +1,53 @@
 # 미완성
 from multiprocessing import Process, Manager, Queue
 import time
+import pandas as pd
 
-def f(q, d, l, num):
-    start = time.time()
+def f(q, d, li, num):
+    d['default'] = 'sucess'
+    # li.append('kim')
+    # data = ('a', 'b', 'c')
+    data = {'c1':'a', 'c2':'b', 'c3':'c'}
+    df = pd.DataFrame(data, index=[0])
+    # df = pd.DataFrame(data, columns=['c1'])
+    print('df', df)
+
+    # li.append(data)
+    li.append(df)
+    # li = data
+
+    # start = time.time()
     # d = {}
-    cnt = 0
-    while True:
-        data = f'test {cnt}'
-        d['default'] = f'test {str(cnt)}'
-        data1 = d['default']
+    # cnt = 0
+    # while True:
+    #     data = f'test {cnt}'
+    #     d['default'] = f'test {str(cnt)}'
+    #     data1 = d['default']
         # d['2'] = 2
         # d[0.25] = None
         # l.reverse()
         # q.put(d)
         # data = q.get()
-        cnt += 1
-        if cnt > num:
-            break
-    # cnt = 0
-    # while True:
-    #     if not d == None:
-    #         save = d
-    #         save1 = l
-    #         q.put(data)
-    #         val = q.get()
-        #
-        # print('값이 있다')
         # cnt += 1
-        # if cnt > 100000:
+        # if cnt > num:
         #     break
-
-    print('소요시간:', time.time() - start)
-    print('크기', len(data))
-    print('data', data)
+    # print('소요시간:', time.time() - start)
+    # print('크기', len(data))
+    # print('data', data)
 
 if __name__ == '__main__':
-    '''
-    num = 1000000
+
+    # with Manager() as manager:
     q = Queue()
-    d = {}
-    l = []
-    p = Process(target=f, args=(q, num))
+    num = 100000
+
+    d = Manager().dict()
+    li = Manager().list()
+    p = Process(target=f, args=(q, d, li, num))
     p.start()
     p.join()
-    # f(d, l, num)
-    # data = q.get()
-    # print(data)
-    with Manager() as manager:
-        num = 1000
-        d = manager.dict()
-        l = manager.list(range(10))
 
-        p = Process(target=f, args=(d, l, num))
-        p.start()
-        p.join()
-
-    '''
-
-    with Manager() as manager:
-        q = Queue()
-        num = 100000
-
-        d = manager.dict()
-        l = manager.list()
-        # d = 1
-        # l = 2
-        p = Process(target=f, args = (q, d, l, num))
-        p.start()
-        p.join()
-
-    # print('q', q.get())
+    print('d', d)
+    print('li', li)
+    print(li[0]['c2'])
+    print(type(li[0]))
